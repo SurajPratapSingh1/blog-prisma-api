d9cfb182fed455b303fa50c6a9ce0011aa0e1878
SurajPratapSingh1 <surajsingh38000@gmail.com>
2025-06-08 21:08:20 +0530

    fix: use generated prisma client from custom output path

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '../generated/prisma/index.js'
 import userRoutes from './routes/users.js'
 import postRoutes from './routes/posts.js'
 
